e333b21c7cddda551c118a75ac3f50cb5d51f55b
Joseph88 <pathetic.sim@gmail.com>
2024-05-21 15:10:05 +0900

    선언과 정의를 분리

--- main.cpp
+++ main.cpp
@@ -2,20 +2,68 @@
 
 using namespace std;
 
+class Circle
+{
+private:
+    int radius;
+
+public:
+    Circle();
+    Circle(int r);
+    int getRadius();
+    double getArea();
+};
+
+Circle::Circle()
+{
+    radius = 1;
+}
+
+Circle::Circle(int r)
+{
+    radius = r;
+}
+
+int Circle::getRadius()
+{
+    return radius;
+}
+
+double Circle::getArea()
+{
+    return 3.14 * radius * radius;
+}
 
 int main(int argc, char const *argv[])
 {
-    int width;
-    int height;
-
-    cout << "너비를 입력하세요 : " << endl;
-    cin >> width;
-    
-    cout << "높이를 입력하세요 : " << endl;
-    cin >> height;
-
-    cout << "면적 : " << width * height << endl;
+    Circle donut;
+    Circle pizza(30);
+    cout << donut.getArea() << endl;
+    cout << pizza.getArea() << endl;
 
     return 0;
 }
 
+
+
+
+
+// #include <iostream>
+
+// using namespace std;
+
+// int main(int argc, char const *argv[])
+// {
+//     int width;
+//     int height;
+
+//     cout << "너비를 입력하세요 : " << endl;
+//     cin >> width;
+
+//     cout << "높이를 입력하세요 : " << endl;
+//     cin >> height;
+
+//     cout << "면적 : " << width * height << endl;
+
+//     return 0;
+// }
