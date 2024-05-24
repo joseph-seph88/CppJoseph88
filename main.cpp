2cbd9174b785930fff0fe8d296715d12f174f5e6
andrew <kihan@snut.ac.kr>
2024-05-24 12:02:35 +0900

    7장 power class생성

--- main.cpp
+++ main.cpp
@@ -1,37 +1,115 @@
 #include <iostream>
-#include "Circle.h"
-#include "Rectangle.h"
+#include <string>
+
 using namespace std;
-void increase(Circle& c)
+class Power
 {
-    int r = c.getRadius();
-    c.setRadius(r + 1);
+private:
+    int punch;
+    int kick;
+
+public:
+    Power(int punch = 0, int kick = 0);
+    ~Power();
+    void show();
+    Power pow(Power &another);
+    Power operator+(Power &another);
+    bool operator==(Power &another);
+    Power& operator+=(Power &another);
+    Power& operator++();
+    Power& operator++(int);
+    Power& operator()(int power);
+    Power& operator<<(int power);
+};
+Power& Power::operator()(int power){
+    this->kick *= power;
+    this->punch *= power;
+    return *this;
 }
-Circle* getCircle()
+
+Power &Power::operator<<(int power)
 {
-    Circle* temp =new Circle(30);
-    return temp;
+    this->kick += power;
+    this->punch += power;
+    return *this;
 }
+
+Power::Power(int punch, int kick)
+{
+    this->punch = punch;
+    this->kick = kick;
+}
+
+Power::~Power()
+{
+}
+
+void Power::show()
+{
+    cout << "punch = " << punch << ", kick = " << kick << endl;
+}
+
+Power Power::pow(Power &another)
+{
+    Power theother;
+    theother.kick = this->kick + another.kick;
+    theother.punch = this->punch + another.punch;
+    return theother;
+}
+
+Power Power::operator+(Power &another)
+{
+    return pow(another);
+}
+
+bool Power::operator==(Power &another)
+{
+    if (this->kick == another.kick && this->punch == another.punch)
+        return true;
+    else
+        return false;
+}
+
+Power &Power::operator+=(Power &another)
+{
+    this->kick += another.kick;
+    this->punch += another.punch;
+    return *this;
+}
+
+Power &Power::operator++()
+{
+    this->kick++;
+    this->punch++;
+    return *this;
+}
+
+Power &Power::operator++(int)
+{
+    // TODO: 여기에 return 문을 삽입합니다.
+}
+
 int main(int argc, char const *argv[])
 {
+    Power p1(3, 5);
+    Power p2(4, 6);
+    Power p3 = p1 + p2;
+    p3.show();
+    if(p1==p2)
+        cout<<"p1 == p2"<<endl;
+    else
+        cout<<"p1 != p2"<<endl;
 
-    Circle waffle(30);
-    waffle.setName("waffle");
-    increase(waffle);
-    cout << waffle.getRadius() << endl;
+    p1+=p2;
+    p1.show();
 
-    Circle pizza(waffle);
-    pizza.setName("pizza");
+    p1(2);
+    p1.show();
 
-    cout << waffle.getName()<< endl;
-    cout << pizza.getName()<< endl;
+    p1<<3<<4<<5;
+    p1.show();
 
-    Circle c;
-    cout<<c.getRadius()<<endl;
-    Circle* c2;
-    c2=getCircle();
-    cout<<c.getRadius()<<endl;
+    
 
-    delete c2;
     return 0;
 }
