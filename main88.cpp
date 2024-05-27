7c604be93b748e29204c067324b17101ea65e3b5
Joseph88 <pathetic.sim@gmail.com>
2024-05-27 11:40:26 +0900

    생성자 소멸자 상속

--- main88.cpp
+++ main88.cpp
@@ -2,61 +2,119 @@
 #include <string>
 using namespace std;
 
-class Point{
-protected:
-    int x, y;
-
+class TV{
+    int size;
 public:
-    void set(int x, int y){
-        this->x = x;
-        this->y = y;
+    TV(){size = 20;}
+    TV(int size){
+        this->size = size;
+        cout<<"TV constructor"<<endl;        
     }
-    void showPoint(){
-        cout << "(" << x << ", " << y << ")" << endl;
+    ~TV() {
+        cout<<"TV Distructor"<<endl;
     }
+    int getSize(){return this->size;}
+
 };
 
-// Point 클래스 상속
-class ColorPoint : public Point{
+class WideTV : public TV{
 private:
-    string color;
+    bool isWide;
 public:
-    void set(int x, int y){
-        this->x = x;
-        this->y = y;
+    WideTV(int size, bool isWide) : TV(size){
+        this->isWide = isWide;
+        cout<<"WideTV constructor"<<endl;
+        }
+    ~WideTV() {
+        cout<<"WideTV Distructor"<<endl;
     }
-    void setColor(string color){
-        this->color = color;
-    }
-    void showColorPoint(){
-        cout << this->color << ": ";
-        showPoint();
-    }
+    bool getIsWide(){return this->isWide;}
 };
 
-class c{
+class smartTV : public WideTV{
+private:
+    string ipAddress;    
 public:
-    void set(int x, int y){
-        this->x = x;
-        this->y = y;        
+    smartTV(int size, bool isWide, string ipAddress) : WideTV(size, isWide){
+        this->ipAddress = ipAddress;
+        cout<<"smartTV constructor"<<endl;
     }
+    ~smartTV() {
+        cout<<"smartTV Distructor"<<endl;
+    }    
+    string getIpAddress(){return this->ipAddress;}
 };
 
 int main(){
-    Point p;
-    ColorPoint cp;
-    p.set(10, 20);
-    
-    cp.set(10, 20);
-    p.showPoint();
-    cp.setColor("Red");
-    cp.showColorPoint();
 
-    ColorPoint *pDer = new ColorPoint();
-    Point *pBase, po;
+    smartTV tv(32, true, "237.84.2.178");
+    cout << tv.getSize() << ", " << tv.getIsWide() << ", " << tv.getIpAddress() << endl;
 
-    pDer->set(3, 4);
-    pDer->setColor("Blue");
 
     return 0;    
 }
+
+
+
+// #include <iostream>
+// #include <string>
+// using namespace std;
+
+// class Point{
+// protected:
+//     int x, y;
+
+// public:
+//     void set(int x, int y){
+//         this->x = x;
+//         this->y = y;
+//     }
+//     void showPoint(){
+//         cout << "(" << x << ", " << y << ")" << endl;
+//     }
+// };
+
+// // Point 클래스 상속
+// class ColorPoint : public Point{
+// private:
+//     string color;
+// public:
+//     void set(int x, int y){
+//         this->x = x;
+//         this->y = y;
+//     }
+//     void setColor(string color){
+//         this->color = color;
+//     }
+//     void showColorPoint(){
+//         cout << this->color << ": ";
+//         showPoint();
+//     }
+// };
+
+// class c{
+// public:
+//     void set(int x, int y){
+//         this->x = x;
+//         this->y = y;        
+//     }
+// };
+
+// int main(){
+//     Point p;
+//     ColorPoint cp;
+//     p.set(10, 20);
+    
+//     cp.set(10, 20);
+//     p.showPoint();
+//     cp.setColor("Red");
+//     cp.showColorPoint();
+
+//     ColorPoint *pDer = new ColorPoint();
+//     Point *pBase, po;
+
+//     pDer->set(3, 4);
+//     pDer->setColor("Blue");
+
+//     return 0;    
+// }
