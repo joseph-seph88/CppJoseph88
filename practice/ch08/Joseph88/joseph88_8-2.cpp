ea73da986829c8c9d35fadc36c2450d2c4b90529
Joseph88 <koutz3434@naver.com>
2024-05-28 02:34:32 +0900

    8장 중

--- practice/ch08/Joseph88/joseph88_8-2.cpp
+++ practice/ch08/Joseph88/joseph88_8-2.cpp
@@ -1,39 +1,53 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
-class Circle{
+class Circle {
     int radius;
 public:
-    Circle(int radius = 0) {this->radius = radius;}
-    int getRadius() {return radius;}
-    void setRadius(int radius) {this->radius = radius;}
-    double getArea() {return 3.14*radius*radius;}
+    Circle(int radius = 0) { this->radius = radius; }
+    int getRadius() { return radius; }
+    void setRadius(int radius) { this->radius = radius; }
+    double getArea() { return 3.14 * radius * radius; }
 };
 
-class NamedCircle : public Circle{
-    string name[5];
+class NamedCircle : public Circle {
+    string name;
+    int radius;
 public:
-    NamedCircle() : Circle(radius) {
-        this->name = name;
+    NamedCircle(int radius, string name) : Circle(radius), name(name) {}
+    NamedCircle() : Circle() {}
+    void show() {
+        cout << "가장 면적이 큰 피자는 " << name << "입니다." << endl;
     }
-    void show(){
-        for(int i=0; i<5; i++){
-            cout<<i+1<<">> "<<getArea()<<" "<<name<<endl;
+    void input(int index) {
+        cout<<index<<">> ";
+        cin>>radius>>name;
+        setRadius(radius);  
+    }
+    int calculNumber(NamedCircle pizza[]){
+        double maxArea = 0;
+        int maxIndex = 0;
+        for (int i = 0; i < 5; i++) {
+            double area = pizza[i].getArea();
+            if (area > maxArea) {
+                maxArea = area;
+                maxIndex = i;
+            }
         }
-    }
-    void put(string name[]){
-        for(int i=0; i<5; i++){
-            this->name[i] = name[i];        
-        } 
+        return maxIndex;
     }
 };
 
-int main(){
+int main() {
     NamedCircle pizza[5];
-    cout<<"5개의 정수 반지름과 원의 이름을 입력하세요"<<endl;
-    cin>>pizza->put(name[]);
-    pizza->show();
-    cout<<"가장 면적이 큰 피자는 블랙홀 피자입니다."<<endl;
+    cout << "5개의 정수 반지름과 피자의 이름을 입력하세요." << endl;
+    
+    for (int i = 0; i < 5; i++) {
+        pizza[i].input(i+1);
+    }
+    int maxIndex = pizza->calculNumber(pizza);
+    pizza[maxIndex].show();
 
     return 0;
 }
