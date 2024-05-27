9fdfa0f7739e5d5938a7788ee4ed24be31cb8298
andrew <kihan@snut.ac.kr>
2024-05-27 16:40:58 +0900

    9-12 동적바인딩 실습

--- main.cpp
+++ main.cpp
@@ -1,54 +1,36 @@
 #include <iostream>
-#include <string>
-
+#include "Circle.h"
+#include "Rect.h"
+#include "Shape.h"
+#include "Line.h"
 using namespace std;
-
-class TV{
-private:
-    int size;
+int main(int argc, char const *argv[])
+{
+    Shape *pStart;
+    Shape *pEnd;
     
-public:
-    TV(){size = 20;}
-    TV(int size){
-        this->size = size;
-        cout<<"TV constructor"<<endl;
-        }
-    ~TV(){
-        cout<<"TV destructor"<<endl;
-    }
-    int getSize(){return this->size;}
-};
-class WideTV : public TV{
-private:
-    bool isWide;
-public:
+    pStart = new Circle();
+    pEnd=pStart;
     
-    WideTV(int size, bool isWide) : TV(size){
-        this->isWide = isWide; 
-        cout<<"WideTV constructor"<<endl;
-        }
-        ~WideTV(){
-            cout<<"WideTV destructor"<<endl;
-        }
-    bool getIsWide(){return this->isWide;}
-};
-
-class smartTV : public WideTV{
-private:
-    string ipAddress;
-public:
-    smartTV(int size, bool isWide, string ipAddress) : WideTV(size, isWide){
-        this->ipAddress = ipAddress;
-        cout<<"smartTV constructor"<<endl;
+    pEnd=pEnd->add(new Rect());
+    pEnd=pEnd->add(new Circle());
+    pEnd=pEnd->add(new Line());
+    pEnd=pEnd->add(new Rect());
+    
+    Shape* p=pStart;
+    while(p!=nullptr){
+        p->paint();
+        p=p->getNext();
     }
-    ~smartTV(){
-        cout<<"smartTV destructor"<<endl;
-    }
-    string getIpAddress(){return this->ipAddress;}
-};
-int main(int argc, const char** argv) {
 
-    smartTV tv(32, true, "237.84.2.178");
-    cout<<tv.getSize()<<" "<<tv.getIsWide()<<" "<<tv.getIpAddress()<<endl;
+    p=pStart;
+    while(p!=nullptr){
+        Shape* temp=p->getNext();
+        delete p;
+        p=temp;
+        
+    }
+
     return 0;
 }
+
